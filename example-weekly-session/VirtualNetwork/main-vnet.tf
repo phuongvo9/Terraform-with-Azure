ea4ee31d84a7e32cde6307a8852ff1a5aae7d088
phuongvo9 <huyphuong4116@gmail.com>
2021-12-14 21:13:21 +0700

    create vnet

--- example-weekly-session/VirtualNetwork/main-vnet.tf
+++ example-weekly-session/VirtualNetwork/main-vnet.tf
@@ -14,3 +14,18 @@ provider "azurerm" {
 
 }
 
+
+resource "azurerum_resource_group" "example" {
+    name = "terraform-resourcegroup"
+    location = "East Asia"
+  
+}
+
+# Create a virtual network in that resource group
+resource "azurerm_virtual_network" "example-vnet" {
+    name = "example-vnet"
+    resource_group_name = azurerum_resource_group.example.name
+    location = azurerum_resource_group.example.location
+    address_space = ["10.0.0.0/16"]
+}
+
