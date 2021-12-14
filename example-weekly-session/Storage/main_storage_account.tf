64977d94ca3f7b98fecd687f83fe14e0dd7063fa
phuongvo9 <huyphuong4116@gmail.com>
2021-12-14 21:44:24 +0700

    add storage account variables

--- example-weekly-session/Storage/main_storage_account.tf
+++ example-weekly-session/Storage/main_storage_account.tf
@@ -19,6 +19,7 @@ provider "azurerm" {
 variable "location" {
   type = string
   default = "East Asia"
+  description = "the default location of the resource"
 }
 
 variable "prefix-name" {
@@ -26,8 +27,40 @@ variable "prefix-name" {
     description = "The prefix that should attach to all resources"
 }
 
+variable "storage_account_kind" {
+  type =string
+  description = "Kind: BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2"
+  default = "StorageV2"
+}
+
+
+variable "storage_account_tier" {
+  type = string
+  description = "Standard and Premium. (BlockBlobStorage and FileStorage accounts only for Premium)"
+  default = "Standard"
+}
+
+variable "storage_account_account_replication_type" {
+    type = string
+    description = "Type of replication: LRS, GRS, RA-GRS, ZRS"
+    default = "LRS"
+}
+
+variable "storage_account_access_tier" {
+    type = string
+    description = "Access Tier: Hot or Cool"
+    default = "Hot"
+}
+
+
 #--------------------- RESOURCE ---------------------------------
 resource "azurerm_resource_group" "example" {
     name = "terraform-resourcegroup"
     location = var.location
+}
+resource "azurerm_storage_account" "example" {
+    name = "${var.prefix-name}-storageaccount"
+    resource_group_name = azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
+
 }
