993e3ba41afaaccdd918890d9a37c48021c00ef5
phuongvo9 <huyphuong4116@gmail.com>
2021-12-14 21:04:05 +0700

    add provider

---
+++ example-weekly-session/VirtualNetwork/main-vnet.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_provider {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "=2.71.0"
+        }
+    }
+}
+
+# Configure the Microsoft Azure Provider
+
+provider "azurerm" {
+  features {
+    
+  }
+}
